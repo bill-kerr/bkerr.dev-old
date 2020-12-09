98e504275913d0c9aa44f0a56fc3912f99a81031
Bill Kerr <48515765+bk7987@users.noreply.github.com>
2020-12-09 11:37:08 -0500

    Setup pages and navigation

---
+++ content/about/_index.md
@@ -0,0 +1,4 @@
+---
+title: About Me
+name: about
+---
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,28 @@ const production = process.env.HUGO_ENVIRONMENT === 'production';
 module.exports = {
   purge: {
     content: ['./layouts/**/*.html'],
-    enabled: !production,
+    enabled: production,
   },
   darkMode: false,
   theme: {
-    extend: {},
+    fontFamily: {
+      display: ['Nunito Sans', 'sans-serif'],
+    },
+    extend: {
+      boxShadow: {
+        'blue-200':
+          'rgb(255, 255, 255) 0px -0.125em 0px 0px inset, rgb(191, 219, 254) 0px -0.5em 0px 0px inset',
+        'blue-100':
+          'rgb(255, 255, 255) 0px -0.125em 0px 0px inset, rgb(219, 234, 254) 0px -0.5em 0px 0px inset',
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      ringColor: ['group-hover'],
+      transitionProperty: ['group-hover'],
+      transitionDuration: ['group-hover'],
+    },
   },
   plugins: [],
 };
