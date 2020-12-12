16bb01d1579729d7123484f63eb9f363081295f6
bk7987 <bk7987@gmail.com>
2020-12-12 11:36:34 -0500

    Added article styling and fixed index centering

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const production = process.env.HUGO_ENVIRONMENT === 'production';
+const { blue } = require('tailwindcss/colors');
 const colors = require('tailwindcss/colors');
 
 module.exports = {
@@ -23,7 +24,7 @@ module.exports = {
     extend: {
       boxShadow: {
         'blue-200':
-          'rgb(255, 255, 255) 0px -0.125em 0px 0px inset, rgb(191, 219, 254) 0px -0.5em 0px 0px inset',
+          'rgb(249, 250, 251) 0px -0.125em 0px 0px inset, rgb(191, 219, 254) 0px -0.5em 0px 0px inset',
         'blue-100':
           'rgb(249, 250, 251) 0px -0.125em 0px 0px inset, rgb(219, 234, 254) 0px -0.5em 0px 0px inset',
       },
@@ -34,6 +35,21 @@ module.exports = {
         github: '#212121',
         linkedin: '#0077b7',
       },
+      typography: {
+        DEFAULT: {
+          css: {
+            a: {
+              boxShadow:
+                'rgb(249, 250, 251) 0px -0.125em 0px 0px inset, rgb(219, 234, 254) 0px -0.5em 0px 0px inset',
+              textDecoration: 'none',
+              '&:hover': {
+                boxShadow:
+                  'rgb(249, 250, 251) 0px -0.125em 0px 0px inset, rgb(191, 219, 254) 0px -0.5em 0px 0px inset',
+              },
+            },
+          },
+        },
+      },
     },
   },
   variants: {
@@ -43,5 +59,5 @@ module.exports = {
       transitionDuration: ['group-hover'],
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 };
